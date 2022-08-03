05ced358cf9e13b0769942520f6aac015a25a77d
hesoyam <user@users-MacBook-Pro.local>
2022-08-03 11:08:45 +0700

    meta

--- meta/index.js
+++ meta/index.js
@@ -14,6 +14,12 @@ export default function Meta({ title, desc }) {
 			<title>{title} - WASAP</title>
 			<meta name='description' content={desc} />
 			<link rel='icon' href='/favicon.ico' />
+
+			<meta property='og:description' content={desc} />
+			<meta property='og:site_name' content={title} />
+			<meta property='og:title' content={title} />
+			<meta property='og:type' content='website' />
+			<meta name='keywords' content='Wasap, Chat seseorang di WhatsApp tanpa simpan nomor, WhatsApp Tool' />
 		</Head>
 	);
 }
